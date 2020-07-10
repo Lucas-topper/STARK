3d3a9ec2dc4ab1fbccf52d547df8bb7541de316a
Lucas-topper <65551118+Lucas-topper@users.noreply.github.com>
2020-07-10 15:25:53 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+1 const Discord - requirel 'discord.js')
+
+2 const client - new Discord.Cluient();
+
+client.on('ready', () » {
+
+console. Log('I an ready!');
+
+6 );
+
+client.on('message', message {
+
+if (nessage.content 'ping') {
+
+message. reply("pong);
+
+10
+
+11
+
+12 ):
+
+13
+
+14 / THIS MUST BE THIS MAY
+
+15 client. Login(process.env. BOT_TOKEN);l
